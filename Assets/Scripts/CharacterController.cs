2380ca5550f21bdb50f3d07327a4220b777ed538
poorprogramming <poorprogramming@yahoo.com>
2015-02-25 18:56:06 -0800

    RPG Idler
    Idle "wood" at your pleasure, and walk around at your own risk.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+
+	public float  speed;
+	
+
+	void FixedUpdate () 
+	{
+
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/WaterController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterController : MonoBehaviour
+{
+	public float scrollSpeed = 0.5F;
+	public GameObject gameObject;
+
+	void Update() 
+	{
+		float offset = Time.time * scrollSpeed;
+		gameObject.renderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}
